ee9f221943c5a893d26b320471e4b30051ec3b84
igor-kaligor <caatro270@gmail.com>
2022-01-21 18:51:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace APP
+{
+    class appBase
+    {
+        static void Main (String[] args)
+        {
+            System.Console.WriteLine("Olá mundo");
+        }
+    }
+
+}
